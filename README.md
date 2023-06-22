ea7eb486426862e3542daccbe67269f16dd92a49
Takumi-Ito-1234 <136666191+Takumi-Ito-1234@users.noreply.github.com>
2023-06-22 20:00:09 +0800

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Getting-and-Cleaning-Data
+
+
+The dataset includes the following files:
+=========================================
+
+- 'README.txt'
+
+- 'features_info.txt': Shows information about the variables used on the feature vector.
+
+- 'features.txt': List of all features.
+
+- 'data.txt': Data set
+
+- 'run_analysis.R': R script that creates above data set
---
+++ run_analysis.R
@@ -0,0 +1,29 @@
+library(dplyr)
+
+#Merges the training and the test sets to create one data set.
+#fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#download.file(fileurl, "./data.zip", method = "curl")
+#unzip("./data.zip")
+
+df_feature <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+df_train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+df_test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+df_all <- rbind(df_train, df_test)
+
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
+TF_mean_std <- grepl("mean\\(\\)", df_feature$V2) | grepl("std\\(\\)", df_feature$V2)
+df_mean_std <- df_all[, TF_mean_std]
+
+#Uses descriptive activity names to name the activities in the data set
+col_name <- df_feature[TF_mean_std, ]
+
+#Appropriately labels the data set with descriptive variable names. 
+names(df_mean_std) <- col_name$V2
+
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+df2 <- lapply(df_mean_std, mean)
+
+write.table(df_mean_std, "./data.txt", row.names = FALSE)
+
+col_name$V1 <- 1:nrow(col_name)
+write.table(col_name, "./feature.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
